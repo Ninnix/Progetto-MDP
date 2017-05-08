83c5c09cb0f671cbb0b06c38f1953ad638407150
Ninnix <nicolo.deva@gmail.com>
2017-05-08 08:37:29 +0200

    eliminata nascita, corretti run dei tipi ATTENZIONE AL WARNING SU SYNCRONIZED

--- src/S.java
+++ src/S.java
@@ -49,6 +49,7 @@ public class S extends Persona{
         }
     }
 
+
     private Persona corteggiamento(){
         //corteggiamento della spregiudicata
         Persona marito = popo.mercato.poll(); //marito sara' null se la coda e' vuota
