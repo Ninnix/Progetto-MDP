4daf552738789a6f791b3bf8b1e84f43998a29a4
SamueleCorsalini <samuelecorsalini@gmail.com>
2017-05-30 16:37:08 +0200

    inserito set sincronizzato al posto della coda del Cenciarelli, ora le donne decrementano la fertilita' agli uomini e sono state inserite stampe per verificare le interazioni sociali dei thread

---
+++ src/SynchroSet.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Samuele on 30/05/2017.
+ */
+public class SynchroSet<E> {
+
+    List<E> spasimanti= new LinkedList<E>();
+
+    private volatile boolean aperto= true;
+
+    public synchronized boolean isEmpty() { return spasimanti.isEmpty(); }
+
+
+    public synchronized void insert(E elem) {
+
+        if (isEmpty()) {
+            spasimanti.add(elem);
+            notifyAll();
+        }
+        else {
+            spasimanti.add(elem);
+        }
+    }
+
+    public synchronized E exctract() throws InterruptedException {
+
+        while (isEmpty()){
+            wait();
+        }
+
+        int indice= new Random().nextInt(spasimanti.size());
+        E result = spasimanti.get(indice);
+        return result;
+    }
+
+    public void chiudi(){
+        this.aperto=false;
+    }
+
+    public boolean isAperto(){
+        return aperto;
+    }
+}
