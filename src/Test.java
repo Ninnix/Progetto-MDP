d683f2e6619aa0054ebf55cbedd0771e077f2c98
SamueleCorsalini <samuelecorsalini@gmail.com>
2017-06-08 16:23:01 +0200

    Cambio nel txt relazione

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,12 @@
+import java.util.Random;
 
 /**
  * Created by nicolo on 29/04/17.
  */
 public class Test {
     public static void main(String[] args) throws InvalidPopulationException {
-        Popolazione p1 = new Popolazione(15, 20 ,3,50, 50, 50, 50);
-        p1.start();
+        //Popolazione p1 = new Popolazione(15, 20 ,3,50, 50, 50, 50);
+        //p1.start();
+        System.out.println(new Random().nextInt(10)+1 );
     }
 }
